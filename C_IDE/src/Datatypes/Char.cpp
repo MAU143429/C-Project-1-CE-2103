5483347d731abdf0d12c087c0ecafad7d20eaf27
Naheem Johnson <60128330+NJohnsonCR@users.noreply.github.com>
2021-03-31 17:21:23 -0600

    Added corresponding data to the char class

--- C_IDE/src/Datatypes/Char.cpp
+++ C_IDE/src/Datatypes/Char.cpp
@@ -3,3 +3,27 @@
 //
 
 #include "Char.h"
+
+string Char::getName() const {
+    return Datatype::getName();
+}
+
+void Char::setName(const string name) {
+    Datatype::setName(name);
+}
+
+int Char::getCount() const {
+    return Datatype::getCount();
+}
+
+void Char::setCount(int count) {
+    Datatype::setCount(count);
+}
+
+char Char::getValue() const {
+    return value;
+}
+
+void Char::setValue(char value) {
+    Char::value = value;
+}
--- C_IDE/src/Datatypes/Char.h
+++ C_IDE/src/Datatypes/Char.h
@@ -4,9 +4,33 @@
 
 #ifndef C_IDE_CHAR_H
 #define C_IDE_CHAR_H
+#include "Datatype.h"
 
+using namespace std;
 
-class Char {
+class Char: public Datatype {
+private:
+    string name;
+    char value;
+    int count;
+
+public:
+    Char(string name, char value, int count){
+        this->name = name;
+        this->value = value;
+        this->count = count;
+    }
+    string getName() const override;
+
+    void setName(string name) override;
+
+    int getCount() const override;
+
+    void setCount(int count) override;
+
+    char getValue() const;
+
+    void setValue(char value);
 
 };
 
