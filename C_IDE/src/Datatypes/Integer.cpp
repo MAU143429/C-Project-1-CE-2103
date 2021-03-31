0cc1a1a03c6b1fb5930064eb7e8295bccb032593
Naheem Johnson <60128330+NJohnsonCR@users.noreply.github.com>
2021-03-31 00:45:04 -0600

    Edited integer class

--- C_IDE/src/Datatypes/Integer.cpp
+++ C_IDE/src/Datatypes/Integer.cpp
@@ -2,4 +2,34 @@
 // Created by njohnson on 3/30/21.
 //
 
+
 #include "Integer.h"
+#include <string>
+
+
+using namespace std;
+
+
+const string Integer::getName() const {
+    return name;
+}
+
+void Integer::setName(const string &name) {
+    Integer::name = name;
+}
+
+int Integer::getValue() const {
+    return value;
+}
+
+void Integer::setValue(int value) {
+    Integer::value = value;
+}
+
+int Integer::getCount() const {
+    return count;
+}
+
+void Integer::setCount(int count) {
+    Integer::count = count;
+}
--- C_IDE/src/Datatypes/Integer.h
+++ C_IDE/src/Datatypes/Integer.h
@@ -1,14 +1,38 @@
 //
 // Created by njohnson on 3/30/21.
-//
 
 #ifndef C_IDE_INTEGER_H
 #define C_IDE_INTEGER_H
+#include <string>
 
+using namespace std;
 
 class Integer {
+private:
+    string name;
+    int value;
+    int count;
+public:
+    Integer(string name, int value, int count){
+        this->name = name;
+        this->value = value;
+        this->count = count;
+    }
+    const string getName() const;
+
+    void setName(const string &name);
+
+    int getValue() const;
+
+    void setValue(int value);
+
+    int getCount() const;
+
+    void setCount(int count);
+
 
 };
+
 
 
 #endif //C_IDE_INTEGER_H
