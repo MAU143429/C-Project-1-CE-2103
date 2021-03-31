c59b4d28a98928c1e54a84507d8c7351e4224125
Naheem Johnson <60128330+NJohnsonCR@users.noreply.github.com>
2021-03-31 17:21:36 -0600

    Added corresponding data to the double class

--- C_IDE/src/Datatypes/Double.cpp
+++ C_IDE/src/Datatypes/Double.cpp
@@ -3,3 +3,27 @@
 //
 
 #include "Double.h"
+
+string Double::getName() const {
+    return Datatype::getName();
+}
+
+void Double::setName(string name) {
+    Datatype::setName(name);
+}
+
+int Double::getCount() const {
+    return Datatype::getCount();
+}
+
+void Double::setCount(int count) {
+    Datatype::setCount(count);
+}
+
+double Double::getValue() const {
+    return value;
+}
+
+void Double::setValue(double value) {
+    Double::value = value;
+}
--- C_IDE/src/Datatypes/Double.h
+++ C_IDE/src/Datatypes/Double.h
@@ -6,8 +6,27 @@
 #define C_IDE_DOUBLE_H
 
 
-class Double {
+#include "Datatype.h"
 
+using namespace std;
+
+class Double: public Datatype {
+private:
+    string name;
+    double value;
+    int count;
+public:
+    string getName() const override;
+
+    void setName(string name) override;
+
+    int getCount() const override;
+
+    void setCount(int count) override;
+
+    double getValue() const;
+
+    void setValue(double value);
 };
 
 
