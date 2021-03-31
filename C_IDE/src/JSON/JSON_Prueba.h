c34d1f2cae8d489a286fc6eb2c217b4b6a28f7fb
Naheem Johnson <60128330+NJohnsonCR@users.noreply.github.com>
2021-03-31 17:22:35 -0600

    Added new directory for json tests

---
+++ C_IDE/src/JSON/JSON_Prueba.h
@@ -0,0 +1,15 @@
+//
+// Created by njohnson on 3/30/21.
+//
+
+#ifndef C_IDE_JSON_PRUEBA_H
+#define C_IDE_JSON_PRUEBA_H
+
+
+class JSON_Prueba {
+public:
+    int JSON();
+};
+
+
+#endif //C_IDE_JSON_PRUEBA_H
